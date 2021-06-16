56a377ae6923943f9620d3f2d8d8f7d382a4d91d
unknown <a-f-r@hotmail.es>
2021-06-16 21:38:12 +0200

    Create files for API routes

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const config = require('config');
+// const config = require('config');
 require('dotenv').config();
 
 const mongoURI = `mongodb+srv://alberto123:${process.env.PASSWORD}@devconnector.dfiu2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
--- server.js
+++ server.js
@@ -7,9 +7,13 @@ connectDB();
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('Server is up and running.');
-});
+app.get('/', (req, res) => res.send('Server is up and running.'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
